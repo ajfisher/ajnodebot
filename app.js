bec702d6f937bb25979af6e5eab4a4c1a12ee611
= <ajfisher.td@gmail.com>
2013-07-20 12:15:47 +1000

    Initial commit of base code

---
+++ app.js
@@ -0,0 +1,32 @@
+var config = require("./config.js"),
+    FourWDBot = require("./lib/4wdbot"),
+    five = require("johnny-five")
+
+var board, robot;
+
+console.info("Setting up robot. Attempting J5 connect to Arduino")
+
+board = new five.Board(config.device);
+
+board.on("ready", function(err) {
+
+    if (err){
+        console.log(err);
+        return;
+    }
+
+    console.info("Board connected. Robot set up");
+
+    robot = new FourWDBot(config.pinout, board);
+
+    console.info("Robot running");
+
+    board.repl.inject({
+        robot: robot
+    });
+
+});
+
+
+// Set up board here.
+
---
+++ config.js
@@ -0,0 +1,48 @@
+var os = require("os");
+
+
+var config = {};
+
+config.host = os.hostname();
+
+config.pinout = {
+    right_motor: {
+        pins: {
+            motor: 3,
+            dir: 12,
+        },
+        thresholds: {
+            max: 100,
+            min: 30
+        }
+    },
+
+    left_motor: {
+        pins: {
+            motor: 11,
+            dir: 13,
+        },
+        thresholds: {
+            max: 100,
+            min: 30,
+        }
+    },
+
+};
+
+
+// if host is ares we're in dev, if it's pallas we're on the robot. The main
+// implication being we have to reset the arduino hardware as we're using
+// UART serial rather than USB serial.
+
+if (config.host == "pallas") {
+    // on the robot
+} else {
+    // in a dev environment
+    config.device = "";
+}
+
+
+
+module.exports = config;
+
