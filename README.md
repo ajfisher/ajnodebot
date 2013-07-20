98dbecba95fd0981942a527eb1f99d086e3d8bab
= <ajfisher.td@gmail.com>
2013-07-20 12:57:17 +1000

    Added CLI and some help details

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-ajnodebot
-=========
+# ajnodebot
 
-4WD RPI + Arduino based node bot .
+4WD RPI + Arduino based nodebot.
+
+# Install
+
+Clone the repository, cd into it and then run npm install
+
+# Running
+
+Two versions are available the full web control version and a more basic CLI
+version that allows you to command the robot over command line.
+
+To run the command line version:
+
+    node app-cli.js
+
+
---
+++ app-cli.js
@@ -0,0 +1,33 @@
+var config = require("./config.js"),
+    FourWDBot = require("./lib/4wdbot"),
+    five = require("johnny-five")
+
+var board, robot;
+
+console.info("Setting up robot. Attempting J5 connect to Arduino")
+
+board = new five.Board(config.device);
+
+board.on("ready", function(err) {
+
+    if (err){
+        console.log(err);
+        return;
+    }
+
+    console.info("Board connected. Robot set up");
+
+    robot = new FourWDBot(config.pinout, board);
+
+    console.info("Robot running issue commands to it");
+
+    board.repl.inject({
+        robot: robot
+    });
+
+    board.repl.start("ajbot:>");
+
+});
+
+
+
